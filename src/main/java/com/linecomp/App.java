11c77f0215f10d27fc57882246919dd3cc20bb01
Akhil-star <akhilakki1305@gmail.com>
2020-09-23 20:19:27 +0530

    Welcome

---
+++ src/main/java/com/linecomp/App.java
@@ -0,0 +1,13 @@
+package com.linecomp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparsion Computation program" );
+    }
+}
